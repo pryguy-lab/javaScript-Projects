bc7c13114cc46fed399773a1df3834c0560b3e9c
Mike Pry <mikepry@hotmail.com>
2023-04-20 06:38:58 -0400

    added button functionality

--- quote-generator/scripts.js
+++ quote-generator/scripts.js
@@ -1,1 +1,39 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
+let apiQuotes = [];
+
+function newQuote() {
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  if (quote.author == "anonymous") {
+    authorText.textContent = "Mr. Awesomeness";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  if (quote.text.length > 200) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
+}
+
+async function getQuotes() {
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {}
+}
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+getQuotes();
