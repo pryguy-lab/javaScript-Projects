5fefe644e5890ea0532e3e1d450838d72855ce2b
Mike Pry <mikepry@hotmail.com>
2023-04-22 07:38:09 -0400

    added loader animation and completed project

--- quote-generator/scripts.js
+++ quote-generator/scripts.js
@@ -5,10 +5,20 @@ const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
 newQuoteBtn.addEventListener("click", newQuote);
 twitterBtn.addEventListener("click", tweetQuote);
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
+function loaderAnimation() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+function removeLoaderAnimation() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
 
 function newQuote() {
+  loaderAnimation();
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   if (quote.author == "Anonymous") {
     authorText.textContent = "Mr. Awesomeness";
@@ -20,14 +30,18 @@ function newQuote() {
   } else {
     quoteText.classList.remove("long-quote");
   }
+
   quoteText.textContent = quote.text;
+  removeLoaderAnimation();
 }
 
 async function getQuotes() {
+  loaderAnimation();
   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
+    console.log(apiQuotes);
     newQuote();
   } catch (error) {}
 }
